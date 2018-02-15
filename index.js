dcaf76ca8178c6b7e7f9a4edf79816d5359c1d89
phytertek <ryan.phytertek@gmail.com>
2018-02-14 20:56:59 -0500

    fix port for deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('build'));
 
-app.listen((process.env.HOST = 3000), error => {
+app.listen((process.env.PORT = 3000), error => {
   if (error) return console.log(error);
   console.log('Static server running...');
 });
